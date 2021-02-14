ebc74f2ea80bca87d74899135270664731c6edd6
torrua <torrua@gmail.com>
2021-02-14 13:18:25 +0700

    Update travis and README

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Loglan Dictionary Database Model for SQLAlchemy
 ![PyPI - Downloads](https://img.shields.io/pypi/dm/Loglan-DB?color=light-green)
 [![Build Status](https://travis-ci.com/torrua/loglan_db.svg?branch=main)](https://travis-ci.com/torrua/loglan_db)
 [![GitHub license](https://img.shields.io/github/license/torrua/loglan_db)](https://github.com/torrua/loglan_db/blob/master/LICENSE)
+[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/torrua/loglan_db/badges/quality-score.png?b=main)](https://scrutinizer-ci.com/g/torrua/loglan_db/?branch=main)
 [![codecov](https://codecov.io/gh/torrua/loglan_db/branch/main/graph/badge.svg?token=10O3OAC180)](https://codecov.io/gh/torrua/loglan_db)
 
-This package is designed to create an LOD dictionary SQL database.
+This package is designed to create a Loglan dictionary SQL database.
--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 # -*- coding: utf-8 -*-
 """Main app page"""
 
+import re
 from loglan_db import app_lod
-from loglan_db.model import Word
+from loglan_db.model import Word, Type
 
 if __name__ == "__main__":
 
     with app_lod().app_context():
-        print(Word.by_name("prukao").first())
+        # type_id = [t.id for t in Type.by(type_filter=["Cpd"]).all()]  # (C)V(V) # "Cpd" (C)V(V)[(C)V(V)]
+
+        afx = Word.query.all()
+        ids = []
+        for a in afx:
+            if re.search("(cdz|cvl|ndj|ndz|dcm|dct|dts|pdz|gts|gzb|svl|jdj|jtc|jts|jvr|tvl|kdz|vts|mzb)", a.name.lower()):
+                # print(a.id_old, a.name, a.origin, a.origin_x)
+                print(a)
+                ids.append(str(a.id_old))
+
+        print(len(ids))
+        print(", ".join(ids))
+        print("|".join(ids))
+    # Parents for Afx = [Prim, Little words]
