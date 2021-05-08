993663b86519362905cc349a70b2f503482e15f8
torrua <torrua@gmail.com>
2021-05-08 18:03:45 +0700

    Update setup

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
   packages=['loglan_db'],
   package_data={'loglan_db': ['*'], },
   include_package_data=True,
-  version='0.1.4',
+  version='0.1.5',
   license='MIT',
   description="Loglan Dictionary Database Model for SQLAlchemy",
   long_description=read("README.md"),
@@ -23,7 +23,7 @@ setup(
   author='torrua',
   author_email='torrua@gmail.com',
   url='https://github.com/torrua/loglan_db',
-  download_url='https://github.com/torrua/loglan_db/archive/v0.1.4.tar.gz',
+  download_url='https://github.com/torrua/loglan_db/archive/v0.1.5.tar.gz',
   keywords=['Loglan', 'Dictionary', 'Database', 'Model', 'LOD'],
   install_requires=[
           'flask', 'sqlalchemy', 'flask_sqlalchemy', 'psycopg2',
