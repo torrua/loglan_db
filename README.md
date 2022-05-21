eb81936a865d20eaaf4b1f62a0a0f68934b52559
torrua <torrua@gmail.com>
2022-05-21 21:51:44 +0700

    🎉 0.1.21

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Loglan Dictionary Database Model for SQLAlchemy
 
 [![Download Loglan-DB](https://img.shields.io/pypi/v/loglan_db.svg)](https://pypi.org/project/Loglan-DB/)
 ![PyPI - Downloads](https://img.shields.io/pypi/dm/Loglan-DB?color=light-green)
+[![Build Status](https://scrutinizer-ci.com/g/torrua/loglan_db/badges/build.png?b=main)](https://scrutinizer-ci.com/g/torrua/loglan_db/build-status/main)
 [![CodeQL](https://github.com/torrua/loglan_db/actions/workflows/codeql-analysis.yml/badge.svg)](https://github.com/torrua/loglan_db/actions/workflows/codeql-analysis.yml)
 [![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/torrua/loglan_db/badges/quality-score.png?b=main)](https://scrutinizer-ci.com/g/torrua/loglan_db/?branch=main)
 [![codecov](https://codecov.io/gh/torrua/loglan_db/branch/main/graph/badge.svg?token=10O3OAC180)](https://codecov.io/gh/torrua/loglan_db)
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
   packages=['loglan_db'],
   package_data={'loglan_db': ['*'], },
   include_package_data=True,
-  version='0.1.20',
+  version='0.1.21',
   license='MIT',
   description="Loglan Dictionary Database Model for SQLAlchemy",
   long_description=read("README.md"),
@@ -23,13 +23,13 @@ setup(
   author='torrua',
   author_email='torrua@gmail.com',
   url='https://github.com/torrua/loglan_db',
-  download_url='https://github.com/torrua/loglan_db/archive/v0.1.20.tar.gz',
+  download_url='https://github.com/torrua/loglan_db/archive/v0.1.21.tar.gz',
   keywords=['Loglan', 'Dictionary', 'Database', 'Model', 'LOD'],
   install_requires=[
           'flask', 'sqlalchemy', 'flask_sqlalchemy', 'psycopg2',
   ],
   classifiers=[
-    'Development Status :: 5 - Production',  # "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
+    'Development Status :: 5 - Production/Stable',  # "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
     'Intended Audience :: Developers',
     'Intended Audience :: Science/Research',
     'Framework :: Flask',
