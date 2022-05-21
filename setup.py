1df81aefdd31b0cafb47e8c1671800687d2d66f8
torrua <torrua@gmail.com>
2022-05-21 20:55:06 +0700

    🎉 Release 0.1.20

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
   packages=['loglan_db'],
   package_data={'loglan_db': ['*'], },
   include_package_data=True,
-  version='0.1.10',
+  version='0.1.20',
   license='MIT',
   description="Loglan Dictionary Database Model for SQLAlchemy",
   long_description=read("README.md"),
@@ -23,21 +23,21 @@ setup(
   author='torrua',
   author_email='torrua@gmail.com',
   url='https://github.com/torrua/loglan_db',
-  download_url='https://github.com/torrua/loglan_db/archive/v0.1.10.tar.gz',
+  download_url='https://github.com/torrua/loglan_db/archive/v0.1.20.tar.gz',
   keywords=['Loglan', 'Dictionary', 'Database', 'Model', 'LOD'],
   install_requires=[
           'flask', 'sqlalchemy', 'flask_sqlalchemy', 'psycopg2',
   ],
   classifiers=[
-    'Development Status :: 4 - Beta',  # "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
+    'Development Status :: 5 - Production',  # "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
     'Intended Audience :: Developers',
     'Intended Audience :: Science/Research',
     'Framework :: Flask',
     'Topic :: Database',
     'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
   ],
-  python_requires='>=3.7',
+  python_requires='>=3.8',
 )
